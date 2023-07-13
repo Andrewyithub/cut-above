ad909a7dc9e625b6b15724f345b161c4dfd09882
Andy-git985 <andy.yu617@gmail.com>
2023-07-12 22:46:08 -0400

    appointment post and schedule put

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -1,4 +1,5 @@
 const scheduleRouter = require('express').Router();
+const Appointment = require('../models/Appointment');
 const Schedule = require('../models/Schedule');
 const date = require('../utils/date');
 
@@ -18,6 +19,20 @@ scheduleRouter.post('/', async (req, res) => {
     message: 'New schedule added',
     data: newSchedules,
   });
+});
+
+scheduleRouter.put('/:id', async (req, res) => {
+  console.log('id: ', req.params.id);
+  console.log('schedule controllers: ', req.body);
+  const { appointment } = req.body;
+  const bookedAppt = await Appointment.findOne({ _id: appointment });
+  const schedule = await Schedule.findOne({ _id: req.params.id });
+  schedule.appointments.push(bookedAppt);
+  await schedule.save();
+
+  res
+    .status(200)
+    .json({ success: true, message: 'Schedule updated', data: schedule });
 });
 
 module.exports = scheduleRouter;
