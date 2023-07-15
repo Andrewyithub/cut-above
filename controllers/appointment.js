a17187cd72c8ece20d6f1e23ac56f12a35825043
Andy-git985 <andy.yu617@gmail.com>
2023-07-15 15:42:21 -0400

    appointment DELETE request

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -1,11 +1,15 @@
 const appointmentRouter = require('express').Router();
 const Appointment = require('../models/Appointment');
+const Schedule = require('../models/Schedule');
 const User = require('../models/User');
 const dateServices = require('../utils/date');
+const dayjs = require('dayjs');
 
 appointmentRouter.get('/', async (req, res) => {
-  const appointment = [{ id: 1, date: '10/08/2023', title: 'Airport' }];
-  res.status(200).json(appointment);
+  const appointments = await Appointment.find({
+    $or: [{ client: req.user }, { employee: req.user }],
+  });
+  res.status(200).json(appointments);
 });
 
 appointmentRouter.post('/', async (req, res) => {
@@ -25,6 +29,26 @@ appointmentRouter.post('/', async (req, res) => {
     success: true,
     message: 'Appointment successfully reserved',
     data: newAppt,
+  });
+});
+
+appointmentRouter.delete('/:id', async (req, res) => {
+  console.log('req.body', req.body);
+  const { employee, date } = await Appointment.findByIdAndDelete(req.params.id);
+  console.log('employee: ', employee);
+  console.log('date: ', date);
+  const scheduleToUpdate = await Schedule.findOne({ date });
+  console.log('scheduleToUpdate', scheduleToUpdate);
+  const index = scheduleToUpdate.appointments.findIndex(
+    (appt) => appt._id.toString() === req.params.id
+  );
+  scheduleToUpdate.appointments.splice(index, 1);
+  // scheduleToUpdate.available.push(employee);
+  await scheduleToUpdate.save();
+
+  res.status(200).json({
+    success: true,
+    message: 'Appointment successfully cancelled',
   });
 });
 
