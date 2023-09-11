1088ae63daa8ab44edd728ee84ea3a45d617b863
Andy-git985 <andy.yu617@gmail.com>
2023-09-10 21:38:58 -0400

    bookAppointment async refactor

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -70,65 +70,8 @@ const bookAppointment = async (req, res) => {
   session.endSession();
 };
 
-module.exports = bookAppointment;
-
-const bookAppointmentV1 = async (req, res) => {
-  const session = await mongoose.startSession();
-  session.startTransaction();
-  try {
-    // Create new appointment
-    const { date, start, end, service, employee } = req.body;
-    const client = await User.findOne({ _id: req.user });
-    const employeeToBook = await User.findOne({ _id: employee });
-    const emailId = emailServices.generateEmailId();
-    const formattedStart = dateServices.easternDateTime(date, start);
-    const newAppt = new Appointment({
-      date: dateServices.easternDate(date),
-      start: formattedStart,
-      end: dateServices.easternDateTime(date, end),
-      service,
-      client,
-      employee: employeeToBook,
-      emailId,
-    });
-    // Validate availability
-    const schedule = await Schedule.findOne({
-      date: dateServices.easternDate(date),
-    }).populate('appointments', 'start end employee');
-    const open = dateServices.checkAvailability(schedule, newAppt);
-    if (!open) {
-      return res.status(500).json({ error: 'Time slot no longer available' });
-    }
-    await newAppt.save({ session });
-
-    // Add to schedule
-    schedule.appointments.push(newAppt);
-    await schedule.save({ session });
-
-    // Send confirmation
-    const emailSent = await emailServices.sendEmail({
-      receiver: client.email,
-      employee: employeeToBook.firstName,
-      date: dateServices.formatDateSlash(date),
-      time: dateServices.formatTime(formattedStart),
-      option: 'confirmation',
-      emailLink: `https://cutaboveshop.fly.dev/appointment/${emailId}`,
-    });
-
-    await session.commitTransaction();
-    res.status(201).json({
-      success: true,
-      message: 'Appointment booked successfully',
-    });
-  } catch (err) {
-    await session.abortTransaction();
-    throw new AppError(500, 'Failed to book appointment.');
-  } finally {
-    session.endSession();
-  }
-};
-//
-
+// ! async refactor
+// ! also add mongo update method
 const modifyAppointment = async (req, res) => {
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -165,6 +108,7 @@ const modifyAppointment = async (req, res) => {
   }
 };
 
+// ! refactor into one
 const updateAppointmentStatus = async (req, res) => {
   const updatedAppointment = await Appointment.updateOne(
     { _id: req.params.id },
