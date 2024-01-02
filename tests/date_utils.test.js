41ad6b10c9b452b44e91dadec7d0884c000138b3
Andy-git985 <andy.yu617@gmail.com>
2024-01-02 03:32:31 -0500

    date util test

---
+++ tests/date_utils.test.js
@@ -0,0 +1,19 @@
+const { formatData } = require('../utils/database');
+const { easternDate } = require('../utils/date');
+
+test('test database util which formats dates to be saved', async () => {
+  const date = '2023-12-24';
+  const start = '10:00';
+  const end = '10:30';
+  const formattedData = formatData(date, start, end);
+  // convert dayjs object to iso string
+  expect(new Date(formattedData.date).toISOString()).toBe(
+    '2023-12-24T05:00:00.000Z'
+  );
+  expect(new Date(formattedData.end).toISOString()).toBe(
+    '2023-12-24T15:30:00.000Z'
+  );
+  expect(new Date(formattedData.start).toISOString()).toBe(
+    '2023-12-24T15:00:00.000Z'
+  );
+});
